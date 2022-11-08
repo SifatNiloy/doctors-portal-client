00c12b9503c8edeccd94577dba9448f1e4af20c0
unknown <sifatniloy18@gmail.com>
2022-11-08 08:27:26 -0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
@@ -8,8 +11,10 @@ function App() {
     <div >
       <Navbar/>
       <Routes>
-        <Route path='/home'>Home</Route>
-        <Route path='/about'>About</Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about'element={<About></About>} ></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*'>About</Route>
       </Routes>
     </div>
   );
