c3e4a4a67f1f58489c820745cae7144fb0082d74
unknown <sifatniloy18@gmail.com>
2022-11-09 06:22:53 -0600

    appointment date seleted on available appointment

--- src/Pages/Home/Appoinement.js
+++ src/Pages/Home/Appoinement.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppointmentBanner from './AppointmentBanner';
+import AvailableAppointments from './AvailableAppointments';
 import Footer from './Footer';
 
 const Appoinement = () => {
+    const [date, setDate]= useState(new Date());
     return (
         <div>
-            <AppointmentBanner></AppointmentBanner>
+            <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
+            <AvailableAppointments date={date} setDate={setDate}></AvailableAppointments>
             <Footer></Footer>
         </div>
     );
--- src/Pages/Home/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner.js
@@ -2,15 +2,22 @@ import React from 'react';
 import chair from '../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-const AppointmentBanner = () => {
+const AppointmentBanner = ({date, setDate}) => {
+    
     return (
         <div className="hero min-h-screen ">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair' />
                 <div>
-                <DayPicker />
+                <DayPicker 
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                />
+                <p>You have Selected: </p>
                 </div>
             </div>
+            
         </div>
     );
 };
