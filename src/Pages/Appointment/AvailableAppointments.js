79c52d21ad61bf1b7bb0086c420ec6b3e9164801
unknown <sifatniloy18@gmail.com>
2022-11-10 00:21:23 -0600

    env variables added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -7,13 +7,13 @@ const AvailableAppointments = ({date}) => {
     const [services, setServices]= useState([]);
     const [treatment, setTreatment]= useState(null);
     useEffect(()=>{
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
         .then(res=> res.json())
         .then(data=> setServices(data))
     },[])
     return (
         <div>
-            <p className='text-xl text-secondary text-center'>Available appointment on {format(date,'PP')}</p>
+            <p className='text-xl text-secondary text-center my-12'>Available appointment on {format(date,'PP')}</p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     services.map(service=> <Service key={service._id} service={service}
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({ service,setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+            <div class="card-body text-center">
+                <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>{
                     slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-500'> Try another day.</span>
                 }
@@ -13,7 +13,7 @@ const Service = ({ service,setTreatment }) => {
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
                 <div class="card-actions justify-center">
                     <label disabled={slots.length===0}
-                    onClick={()=>setTreatment(service)} for="booking-modal" class="btn btn btn-secondary uppercase">Book Appointment</label>
+                    onClick={()=>setTreatment(service)} for="booking-modal" class="btn btn-sm btn-secondary uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
                 </div>
             </div>
         </div>
