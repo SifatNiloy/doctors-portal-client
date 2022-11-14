87318a9faf2fc84e6b35725610f09be5e75571d3
unknown <sifatniloy18@gmail.com>
2022-11-14 15:01:27 -0600

    admin update success showed in ui

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({user}) => {
-    const {email}= user;
+const UserRow = ({user, refetch}) => {
+    const {email, role}= user;
     const makeAdmin=()=>{
         fetch(`http://localhost:5000/user/admin/${email}`,{
             method: 'PUT',
@@ -11,7 +12,8 @@ const UserRow = ({user}) => {
         })
         .then(res=> res.json())
         .then(data=>{
-            console.log(data)
+            refetch();
+            toast.success(`Successfully made an admin`);
         })
     }
     return (
@@ -19,7 +21,7 @@ const UserRow = ({user}) => {
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td><button onClick={makeAdmin} className="btn btn-xs">Make Admin</button></td>
+            <td>{role!=='admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
             <td><button className="btn btn-xs">Remove User</button></td>
         </tr>
     );
